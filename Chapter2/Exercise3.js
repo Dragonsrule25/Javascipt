ff9a6cd7ad5be091e2d3587468813784c7162945
Olivia Murray <157065495+Dragonsrule25@users.noreply.github.com>
2024-09-19 10:38:00 -0700

    Update Exercise3.js

--- Chapter2/Exercise3.js
+++ Chapter2/Exercise3.js
@@ -4,6 +4,7 @@ var makeGrid = function(l, w){
     for(i = 1; i <= l; i++){
       if (i % 2 == 0 || i == 2){
         console.log(even);
+          
       }
       else{
         console.log(odd);
