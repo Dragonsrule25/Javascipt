51747df939fa16514f6710c9d1d91c7c6c38402f
Olivia Murray <157065495+Dragonsrule25@users.noreply.github.com>
2024-09-19 10:36:14 -0700

    Update Exercise3.js

--- Chapter2/Exercise3.js
+++ Chapter2/Exercise3.js
@@ -1,7 +1,6 @@
 var makeGrid = function(l, w){
     var odd = " #".repeat(w);
     var even = "# ".repeat(w);
-    
     for(i = 1; i <= l; i++){
       if (i % 2 == 0 || i == 2){
         console.log(even);
