86b0bc77cb2f857f8f1ae7b05c39ef2a4ceb0179
Olivia Murray <157065495+Dragonsrule25@users.noreply.github.com>
2024-09-19 10:34:51 -0700

    Add files via upload

---
+++ Chapter2/Exercise3.js
@@ -0,0 +1,13 @@
+var makeGrid = function(l, w){
+    var odd = " #".repeat(w);
+    var even = "# ".repeat(w);
+    
+    for(i = 1; i <= l; i++){
+      if (i % 2 == 0 || i == 2){
+        console.log(even);
+      }
+      else{
+        console.log(odd);
+      }
+    }
+  }
